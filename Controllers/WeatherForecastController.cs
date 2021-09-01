e825a6456dfc10afbbe93bcdce5c063daf79a0d0
ionutroth <87963655+ionutroth@users.noreply.github.com>
2021-09-01 12:38:55 +0300

    ConvertResponseToWeatherForecast method created

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RestSharp;
 
 namespace ApiSandbox.Controllers
 {
@@ -26,6 +27,17 @@ namespace ApiSandbox.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            var client = new RestClient("https://api.openweathermap.org/data/2.5/onecall?lat=11&lon=11&exclude=hourly,minutely&appid=7ad9707743286cc164f725a3cd3d3c6e");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+            return ConvertResponseToWeatherForecast(response.Content);
+
+            
+        }
+        private IEnumerable<WeatherForecast> ConvertResponseToWeatherForecast(string content)
+        {
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
@@ -35,6 +47,7 @@ namespace ApiSandbox.Controllers
             })
             .ToArray();
         }
+
         //http://api.openweathermap.org/data/2.5/forecast/daily?lat=44.4268&lon=26.1025&cnt=1&appid=7ad9707743286cc164f725a3cd3d3c6e
     }
 }
