5fa89e041c867c54342234371086552f14e2baf4
ionutroth <87963655+ionutroth@users.noreply.github.com>
2021-09-23 10:46:16 +0300

    Edited user controller

--- UserManagement/Data/ApplicationDbContext.cs
+++ UserManagement/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using UserManagement.Models;
 
 namespace UserManagement.Data
 {
--- UserManagement/Startup.cs
+++ UserManagement/Startup.cs
@@ -29,7 +29,7 @@ namespace UserManagement
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseNpgsql(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("Default")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
